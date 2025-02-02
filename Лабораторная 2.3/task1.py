c25599c7bda79117b58ede00847f6301c8a81c82
PolinaBalezina <pbalezina@gmail.com>
2025-02-02 19:31:56 +0300

    Лабораторная работа 3

---
+++ Лабораторная 2.3/task1.py
@@ -0,0 +1,68 @@
+class Book:
+    """ Базовый класс книги. """
+
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def author(self) -> str:
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages  # здесь вызывается свойство
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, value: int):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Количество страниц должно быть положительным целым числом.")
+        self._pages = value
+
+    def __str__(self):
+        return f"Бумажная книга: {self.name}. Автор {self.author}. Страниц: {self.pages}"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration  # здесь вызывается свойство
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, value: float):
+        if not isinstance(value, (float, int)) or value <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом.")
+        self._duration = float(value)
+
+    def __str__(self):
+        return f"Аудиокнига: {self.name}. Автор {self.author}. Длительность: {self.duration} ч."
+
+
+# Примеры использования
+paper_book = PaperBook("1984", "Джордж Оруэлл", 328)
+audio_book = AudioBook("451 градус по Фаренгейту", "Рэй Брэдбери", 11.5)
+
+print(paper_book)
+print(repr(paper_book))
+print(audio_book)
+print(repr(audio_book))
